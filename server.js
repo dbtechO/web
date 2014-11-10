c517f8a40ed6df4d21dcd5532d78b15cb72e5048
William Guss <carlostnb@gmail.com>
2014-11-09 23:07:08 -0700

    Setting up full ember implementation

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.get('/', function (req, res) {
     res.render('home' );
 });
 
-app.get('/client.js', express.static(__dirname + 'client.js'))
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
