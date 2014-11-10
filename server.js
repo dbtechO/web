a5f9b4e07d725b49ac65f6b022b8391a2c87be5c
William Guss <carlostnb@gmail.com>
2014-11-09 22:45:22 -0700

    Making dedicated client reference

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.get('/', function (req, res) {
     res.render('home' );
 });
 
+app.get('/app.js', express.static(__dirname + 'app.js'))
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
