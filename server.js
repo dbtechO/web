f8d890b48e04ff98fd88125f75eae22a48ad54de
Phillip Kuznetsov <superpker@gmail.com>
2014-11-15 09:47:17 -0700

    Moved the slider to its own class so it can be called with the same handlebars shortcut

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require('express'),
     jade = require('jade');
 
 var app = express();
-
 app.set('port', (process.env.PORT || 8080))
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'jade');
@@ -13,7 +12,6 @@ app.use('/styles', express.static(__dirname + '/styles'));
 app.get('/', function (req, res) {
     res.render('home' );
 });
-
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
--- styles/client.js
+++ styles/client.js
@@ -1,8 +1,12 @@
 App = Ember.Application.create();
 App.Router.map(function() {
-	this.resource('index');
 	this.resource('about');
 	this.resource('projects');
 })
 
 $('.carousel').carousel()
+
+// var source = $("#project-slider").html();
+// var template= Handlebars.compile(source);
+// $('#projects').append(template());
+ Handlebars.registerPartial("project-slider", $("#project-slider").html());
